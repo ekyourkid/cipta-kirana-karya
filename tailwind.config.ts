e810b2855875e89a7a191a54796b782236475fb8
ekyourkid <eky1579@gmail.com>
2024-06-25 20:30:45 +0700

    feat: typing on tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -37,7 +37,7 @@ const config: Config = {
         },
     },
     plugins: [
-        function ({ addUtilities }) {
+        function ({ addUtilities }: any) {
             const newUtitlities = {
                 ".no-scrollbar::-webkit-scrollbar": {
                     display: "none",
